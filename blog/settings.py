469abb336ab66f197666f23936630ebf02f2769a
Eric <developer.ericgitangu@gmail.com>
2024-08-21 14:50:20 -0500

    ...

--- blog/settings.py
+++ blog/settings.py
@@ -155,6 +155,9 @@ else:
     # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
     STATIC_ROOT = 'staticfiles/'
+    STATICFILES_DIRS = [
+         BASE_DIR / 'static',
+     ]
     STATIC_URL = 'static/'
 
 # STORAGES = {
